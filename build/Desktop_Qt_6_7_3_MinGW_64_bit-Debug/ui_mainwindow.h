6c03fd2a8d402ecf697891113a6ba6994faf3b3b
hamma-m2k <marzouguim066@gmail.com>
2025-10-07 23:00:46 +0100

    initial

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,226 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QListView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
+    QLineEdit *lineEdit_3;
+    QLineEdit *lineEdit_4;
+    QLineEdit *lineEdit_5;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QListView *listView;
+    QLabel *label_6;
+    QLineEdit *lineEdit_6;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QGroupBox *groupBox;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_10;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1331, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("QWidget { \n"
+"    background-color: rgb(245, 245, 220); \n"
+"} \n"
+"\n"
+"\n"
+"QLineEdit {\n"
+"    background-color: white;\n"
+"    border: 1px solid #cccccc;\n"
+"    border-radius: 4px;\n"
+"    padding: 4px;\n"
+"    color: black;\n"
+"}\n"
+"\n"
+"QLineEdit:focus {\n"
+"    border: 2px solid #009688;\n"
+"}\n"
+"\n"
+"\n"
+"QListView {\n"
+"    background-color: white;\n"
+"    border: 1px solid #cccccc;\n"
+"    border-radius: 4px;\n"
+"    color: black;\n"
+"}\n"
+"QPushButton { \n"
+"    background-color: #009688; \n"
+"    color: white; \n"
+"    border: none; \n"
+"    border-radius: 6px; \n"
+"    padding: 6px 12px; \n"
+"    font-weight: bold; \n"
+"} \n"
+"\n"
+"QPushButton:hover { \n"
+"    background-color: #00796B; \n"
+"} \n"
+"\n"
+"QPushButton:pressed { \n"
+"    background-color: #004D40; \n"
+"} \n"
+"\n"
+"QLabel { \n"
+"    color: #333333; \n"
+"    font-size: 14px; \n"
+"    font-weight: bold; \n"
+"}"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(470, 420, 161, 29));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(470, 40, 151, 28));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName("lineEdit_2");
+        lineEdit_2->setGeometry(QRect(470, 120, 151, 28));
+        lineEdit_3 = new QLineEdit(centralwidget);
+        lineEdit_3->setObjectName("lineEdit_3");
+        lineEdit_3->setGeometry(QRect(470, 190, 151, 28));
+        lineEdit_4 = new QLineEdit(centralwidget);
+        lineEdit_4->setObjectName("lineEdit_4");
+        lineEdit_4->setGeometry(QRect(470, 260, 151, 28));
+        lineEdit_5 = new QLineEdit(centralwidget);
+        lineEdit_5->setObjectName("lineEdit_5");
+        lineEdit_5->setGeometry(QRect(470, 330, 151, 28));
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(370, 40, 63, 20));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(370, 120, 63, 20));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(370, 190, 63, 20));
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName("label_4");
+        label_4->setGeometry(QRect(370, 270, 71, 20));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(370, 340, 63, 20));
+        listView = new QListView(centralwidget);
+        listView->setObjectName("listView");
+        listView->setGeometry(QRect(860, 200, 401, 241));
+        label_6 = new QLabel(centralwidget);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(1010, 10, 131, 20));
+        lineEdit_6 = new QLineEdit(centralwidget);
+        lineEdit_6->setObjectName("lineEdit_6");
+        lineEdit_6->setGeometry(QRect(970, 40, 181, 28));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(920, 130, 101, 29));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(1090, 130, 111, 29));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(910, 480, 121, 29));
+        pushButton_5 = new QPushButton(centralwidget);
+        pushButton_5->setObjectName("pushButton_5");
+        pushButton_5->setGeometry(QRect(1120, 480, 131, 29));
+        pushButton_6 = new QPushButton(centralwidget);
+        pushButton_6->setObjectName("pushButton_6");
+        pushButton_6->setGeometry(QRect(980, 90, 131, 29));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(0, -30, 191, 591));
+        pushButton_7 = new QPushButton(groupBox);
+        pushButton_7->setObjectName("pushButton_7");
+        pushButton_7->setGeometry(QRect(30, 130, 111, 29));
+        pushButton_8 = new QPushButton(groupBox);
+        pushButton_8->setObjectName("pushButton_8");
+        pushButton_8->setGeometry(QRect(30, 260, 111, 29));
+        pushButton_9 = new QPushButton(groupBox);
+        pushButton_9->setObjectName("pushButton_9");
+        pushButton_9->setGeometry(QRect(30, 380, 111, 29));
+        pushButton_10 = new QPushButton(groupBox);
+        pushButton_10->setObjectName("pushButton_10");
+        pushButton_10->setGeometry(QRect(30, 500, 111, 29));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1331, 25));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "ajouter condidat", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Nom", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Prenom", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "CIN", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Telephone", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Mail", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "Entrer CIN", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "modifier ", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "supprimer", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Afficher tous", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Supprimer tous", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "Recherche", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "GroupBox", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "Condidats", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "Moniteur", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "V\303\251hicule", nullptr));
+        pushButton_10->setText(QCoreApplication::translate("MainWindow", "Admin", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
